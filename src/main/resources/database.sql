7060c2c8f5c7f4db29f2641d55b7d4e43b53f4e2
Sasakin Maxim <max_to_day@Mail.ru>
2017-02-21 02:18:30 +0300

    pom.xml and database tables was creating

---
+++ src/main/resources/database.sql
@@ -0,0 +1,33 @@
+-- Table users
+CREATE TABLE users (
+  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  pasword  VARCHAR(255) NOT NULL
+)
+  ENGINE InnoDB;
+
+-- Table rolles
+CREATE TABLE roles (
+  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role VARCHAR(100) NOT NULL
+)
+  ENGINE InnoDB;
+
+-- Table user_roles
+CREATE TABLE user_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id),
+
+  UNIQUE (user_id, role_id)
+)
+  ENGINE InnoDB;
+
+-- Insert data
+INSERT INTO users VALUES (1,'Vladimir','112113');
+INSERT INTO roles VALUES (1,'ROLE_USER');
+INSERT INTO roles VALUES (2,'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUES (1,2);
